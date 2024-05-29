4a25343461db852c810c7042b929058e142b0ecf
ranjbarmahdi <ranmahdi66@gmail.com>
2024-05-29 07:01:32 -0700

    v

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ async function scrap(page, productId) {
           console.log(`======================== Start scraping : \n${productURL}\n`);
           await page.goto(productURL, { timeout: 180000 });
 
-          await delay(Math.random(7000));
+          await delay(Math.random(10000));
 
           const html = await page.content();
           const $ = await cheerio.load(html);
