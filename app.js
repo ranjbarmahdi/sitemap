6962deb97b1f68e552d978833dbd41d666f96001
ranjbarmahdi <ranmahdi66@gmail.com>
2024-05-29 08:24:30 -0700

    v

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ async function scrap(page, productId) {
           console.log(`======================== Start scraping : \n${productURL}\n`);
           await page.goto(productURL, { timeout: 180000 });
 
-          await delay(Math.random(2000));
+          await delay(Math.random(10000));
 
           
           try {
